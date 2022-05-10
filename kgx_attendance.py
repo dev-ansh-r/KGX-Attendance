8d70b9d400f7dbeb38d626b400291e60439c00dd
Devansh <devansh@pop-os.localdomain>
2022-05-10 17:04:17 +0530

    added the logic to the code

--- kgx_attendance.py
+++ kgx_attendance.py
@@ -1,13 +1,17 @@
+from datetime import datetime
 import requests
 import json
 
 rfid = input()
-url = "https://intent-sunbeam-40.hasura.app/v1/graphql"
- 
+url = "https://intent-sunbeam-40.hasura.app/api/rest/entry"
+
 headers = {"Content-Type": "application/json; charset=utf-8", "x-hasura-admin-secret" : "4E9fBl6pQoyEL138Ov9jmoY3xnKtMpKm2KtrHWHPOUdcXzMHBzvII9CDooZZH5Ay"}
 
 data = {
-    "rfid_key": rfid,
+    "attendance":{
+        "rfid_key": rfid,
+        "in_time" : "2003-04-12 04:05:06"
+    }
 }
 
 response = requests.post(url, headers=headers, json=data)
