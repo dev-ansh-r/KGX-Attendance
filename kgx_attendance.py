10ec54e0ed000f05ec4715db5e9e0143a1682e03
Devansh <devansh@pop-os.localdomain>
2022-05-10 17:07:35 +0530

    added the logic to the code

--- kgx_attendance.py
+++ kgx_attendance.py
@@ -1,22 +1,38 @@
 from datetime import datetime
 import requests
 import json
+d =	{}
+key = input()
 
-rfid = input()
-url = "https://intent-sunbeam-40.hasura.app/api/rest/entry"
+url_post = "https://intent-sunbeam-40.hasura.app/api/rest/entry"
+url_update = "https://intent-sunbeam-40.hasura.app/api/rest/update_attendance"
+
 
 headers = {"Content-Type": "application/json; charset=utf-8", "x-hasura-admin-secret" : "4E9fBl6pQoyEL138Ov9jmoY3xnKtMpKm2KtrHWHPOUdcXzMHBzvII9CDooZZH5Ay"}
 
-data = {
+data_in_time = {
     "attendance":{
-        "rfid_key": rfid,
+        "rfid_key": key,
         "in_time" : "2003-04-12 04:05:06"
     }
 }
 
-response = requests.post(url, headers=headers, json=data)
+data_out_time = {
+    "id": 37,
+    "out_time": "2003-04-12 04:05:06"
+}
+
+if key in d:
+  update = requests.patch(url_update, headers=headers, json=data_out_time)
+  del d[key]
+else:
+    response = requests.post(url_post, headers=headers, json=data_in_time)
+    d[key].append(response)
+
  
 print("Status Code", response.status_code)
 print("JSON Response ", response.json())
 
+print(d)
 
+  
